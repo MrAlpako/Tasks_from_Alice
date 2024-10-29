e52997f2c678467376725401f1781f60f9d0b9cf
Sergey <124809137+MrAlpako@users.noreply.github.com>
2024-10-29 18:06:41 +0300

    Add files via upload

---
+++ Alice_1_the_average_is_arithmetic.py
@@ -0,0 +1,23 @@
+﻿
+list_num = []
+print('Для вычисления среднего значения введите "q"')
+print('____________\n')
+
+while True:
+    try:
+        while True:
+            i = input('Введите значение: ')
+            if i != 'q':
+                list_num.append(int(i))
+            else:
+                mess = 'Вычисляется среднее занчение...'
+                print('\n##############')
+                print(mess.strip())
+                print('##############\n')
+                print(f'Среднее значение = {sum(list_num)/len(list_num)}\n')
+                break
+    except ValueError:
+        print('Неверный формат Ввода..')
+        
+    
+
